3ae0411f390b93b7faa31811d2d37aed3fc57386
Bartek <bbogucki7@gmail.com>
2024-06-24 11:33:08 +0200

    finished log-reg view

---
+++ src/components/LogReg.jsx
@@ -0,0 +1,14 @@
+
+
+export default function LogReg() {
+	return (
+		<div>
+			<div className='log-reg'>
+				<div className='log-reg__main-container'>
+					<div className='log-reg__container'>LOGIN</div>
+					<div className='log-reg__container'>REGISTER</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/components/Reg.jsx
@@ -0,0 +1,3 @@
+export default function Reg() {
+	return <div></div>;
+}
