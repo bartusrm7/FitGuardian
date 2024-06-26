723c66d27635912b1cecf123fff46f67fb9e3785
Bartek <bbogucki7@gmail.com>
2024-06-26 08:58:45 +0200

    added hamburger menu and setted display flex on this

--- fit-guardian-frontend/src/components/Dashboard.jsx
+++ fit-guardian-frontend/src/components/Dashboard.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 export default function Dashboard() {
+	const [hamburger, setHamgurger] = useState(false);
+
+	const handleHamburger = () => setHamgurger(!hamburger);
+
 	return (
 		<div>
-			<div className='dashboard'></div>
+			<div className='dashboard'>
+				<div className='dashboard__main-container'>
+					<div className='dashboard__container'>
+						<div className='dashboard__container-name'>
+							<h3 className='dashboard__label'>FitGuardian</h3>
+							{hamburger ? (
+								<span className='material-symbols-outlined hamburger' onClick={handleHamburger}>
+									menu
+								</span>
+							) : (
+								<span className='material-symbols-outlined hamburger' onClick={handleHamburger}>
+									close
+								</span>
+							)}
+						</div>
+						<div className={`dashboard__container-menu ${hamburger ? "opened-menu" : ""}`}>
+							<div className='dashboard__menu-items'>Menu</div>
+							<div className='dashboard__menu-items'>Recipes</div>
+							<div className='dashboard__menu-items'>Statistics</div>
+							<div className='dashboard__menu-items'>Settings</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
