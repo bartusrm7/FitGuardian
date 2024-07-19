531b715ea4e93295b276e7c48833b17e5d9997e1
Bartek <bbogucki7@gmail.com>
2024-07-19 19:49:32 +0200

    changed statistics layout, started set date choosing by user

--- fit-guardian-frontend/src/components/Statistics.jsx
+++ fit-guardian-frontend/src/components/Statistics.jsx
@@ -6,26 +6,14 @@ import Dashboard from "./Dashboard";
 
 export default function Statistics() {
 	const { userTotalCalories, setUserTotalCalories, userTotalMacros, setUserTotalMacros } = useUserContext();
-	const { setUserMeal, setAllMacros, currentDate, setCurrentDate } = useFoodContext();
+	const { userMeals, setUserMeal, setAllMacros } = useFoodContext();
 	const [periodChoice, setPeriodChoice] = useState("");
-	const [currentStatisticsDate, setCurrentStatisticsDate] = useState([]);
 	const [allMacrosPercentageCompleted, setAllMacrosPercentageCompleted] = useState({
 		calories: 0,
 		proteins: 0,
 		carbs: 0,
 		fats: 0,
 	});
-	const handleCurrentStatisticsDate = () => {
-		const newDate = new Date();
-		const formattedDate = newDate.toISOString().split("T")[0];
-		setCurrentStatisticsDate(formattedDate);
-	};
-	const handleChangeStatisticsDate = direction => {
-		const newDate = new Date(currentStatisticsDate);
-		newDate.setDate(newDate.getDate() + direction);
-		const formattedDate = newDate.toISOString().split("T")[0];
-		setCurrentStatisticsDate(formattedDate);
-	};
 	const getDateFromPeriodChoiceDate = () => {
 		const date = new Date();
 		if (periodChoice === "week") {
@@ -37,6 +25,43 @@ export default function Statistics() {
 		}
 		return date;
 	};
+	const filteredMealsLastPeriodTimes = () => {
+		const newDate = new Date();
+		const lastWeek = new Date(newDate);
+		const lastMonth = new Date(newDate);
+		const lastYear = new Date(newDate);
+		lastWeek.setDate(newDate.getDate() - 7);
+		lastMonth.setMonth(newDate.getMonth() - 1);
+		lastYear.setFullYear(newDate.getFullYear() - 1);
+
+		const newAllMacrosPercentageCompleted = {
+			totalCalories: 0,
+			totalProteins: 0,
+			totalCarbs: 0,
+			totalFats: 0,
+		};
+
+		const getUserMeals = localStorage.getItem("userMeals");
+		const getUpdatedUserMeals = JSON.parse(getUserMeals);
+
+		getUpdatedUserMeals.forEach(meals => {
+			meals.food.forEach(meal => {
+				const totalCalories = parseFloat(meal.foodCalories);
+				const totalProteins = parseFloat(meal.foodProteins);
+				const totalCarbs = parseFloat(meal.foodCarbs);
+				const totalFats = parseFloat(meal.foodFats);
+			});
+		});
+		setAllMacrosPercentageCompleted({
+			calories: totalCalories,
+			proteins: totalProteins,
+			carbs: totalCarbs,
+			fats: totalFats,
+		});
+	};
+
+	// USTAWIĆ ABY PO KLIKNIĘCIU W NASZ BUTTON MOŻNA BYŁO WYŚWIETLIĆ ODPOWIEDNIO TO CO CHCEMY (WEEK,MONTH,YEAR)
+	
 	useEffect(() => {
 		const updatedUserMealsString = localStorage.getItem("userMeals");
 		if (updatedUserMealsString) {
@@ -49,9 +74,8 @@ export default function Statistics() {
 			setAllMacros(userAllMacros);
 		}
 		getDateFromPeriodChoiceDate();
-		handleCurrentStatisticsDate();
+		filteredMealsLastPeriodTimes();
 	}, []);
-	console.log(allMacrosPercentageCompleted);
 
 	return (
 		<div>
@@ -60,77 +84,57 @@ export default function Statistics() {
 					<div className='statistics__container'>
 						<div className='statistics__container-name'>
 							<h3 className='statistics__label'>Statistics</h3>
-							<span
-								className='material-symbols-outlined arrows-left arrows'
-								onClick={() => handleChangeStatisticsDate(-1)}>
-								keyboard_double_arrow_left
-							</span>
-							<input
-								className='statistics__date'
-								type='date'
-								value={currentStatisticsDate}
-								onChange={e => setCurrentStatisticsDate(e.target.value)}
-							/>
-							<span
-								className='material-symbols-outlined arrows-right arrows'
-								onClick={() => handleChangeStatisticsDate(1)}>
-								keyboard_double_arrow_right
-							</span>
+						</div>
+						<div className='statistics__calendar-container'>
+							<button className='statistics__date' onClick={() => setPeriodChoice("week")}>
+								Last Week
+							</button>
+							<button className='statistics__date' onClick={() => setPeriodChoice("month")}>
+								Last Month
+							</button>
+							<button className='statistics__date' onClick={() => setPeriodChoice("year")}>
+								Last Year
+							</button>
 						</div>
 						<div className='statistics__calories-container'>
-							<div className='statistics__calendar-container'></div>
-
 							<div className='statistics__main-view-container'>
-								<div className='statistics__calories-container'>
-									<div className='statistics__name'>{userTotalCalories}cal</div>
-									<div className='statistics__circle'>
-										<Circle
-											strokeWidth={11}
-											strokeColor={"#000046"}
-											trailWidth={11}
-											trailColor={"#60dfff"}
-											gapDegree={30}></Circle>
-										<div className='statistics__macro-name'>{allMacrosPercentageCompleted.calories.toFixed(1)}%</div>
+								<div className='statistics__macros-progress'>
+									<div className='statistics__item'>
+										<div className='statistics__all-macro-name'>
+											All calories statistics: <span>{`123cal`}</span>
+										</div>
+										<div className='statistics__average-macro-name'>
+											All calories average statistics: <span>{`123cal`}</span>
+										</div>
 									</div>
 								</div>
-								<div className='statistics__macros-container'>
-									<div className='statistics__macros-circle-progress'>
-										<div className='statistics__name'>{userTotalMacros.proteins}g</div>
-										<div className='statistics__circle'>
-											<Circle
-												percent={allMacrosPercentageCompleted.proteins}
-												strokeWidth={11}
-												strokeColor={"#000046"}
-												trailWidth={11}
-												trailColor={"#60dfff"}
-												gapDegree={30}></Circle>
-											<div className='statistics__macro-name'>{allMacrosPercentageCompleted.proteins.toFixed(1)}%</div>
+								<div className='statistics__macros-progress'>
+									<div className='statistics__item'>
+										<div className='statistics__all-macro-name'>
+											All proteins statistics: <span>{`123g`}</span>
+										</div>
+										<div className='statistics__average-macro-name'>
+											All proteins average statistics: <span>{`123g`}</span>
 										</div>
 									</div>
-									<div className='statistics__macros-circle-progress'>
-										<div className='statistics__name'>{userTotalMacros.carbs}g</div>
-										<div className='statistics__circle'>
-											<Circle
-												percent={allMacrosPercentageCompleted.proteins}
-												strokeWidth={11}
-												strokeColor={"#000046"}
-												trailWidth={11}
-												trailColor={"#60dfff"}
-												gapDegree={30}></Circle>
-											<div className='statistics__macro-name'>{allMacrosPercentageCompleted.carbs.toFixed(1)}%</div>
+								</div>
+								<div className='statistics__macros-progress'>
+									<div className='statistics__item'>
+										<div className='statistics__all-macro-name'>
+											All carbs statistics: <span>{`123g`}</span>
+										</div>
+										<div className='statistics__average-macro-name'>
+											All carbs average statistics: <span>{`123g`}</span>
 										</div>
 									</div>
-									<div className='statistics__macros-circle-progress'>
-										<div className='statistics__name'>{userTotalMacros.fats}g</div>
-										<div className='statistics__circle'>
-											<Circle
-												percent={allMacrosPercentageCompleted.proteins}
-												strokeWidth={11}
-												strokeColor={"#000046"}
-												trailWidth={11}
-												trailColor={"#60dfff"}
-												gapDegree={30}></Circle>
-											<div className='statistics__macro-name'>{allMacrosPercentageCompleted.fats.toFixed(1)}%</div>
+								</div>
+								<div className='statistics__macros-progress'>
+									<div className='statistics__item'>
+										<div className='statistics__all-macro-name'>
+											All fats statistics: <span>{`123g`}</span>
+										</div>
+										<div className='statistics__average-macro-name'>
+											All fats average statistics: <span>123</span>
 										</div>
 									</div>
 								</div>
